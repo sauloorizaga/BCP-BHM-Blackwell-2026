c4ba086be5fa0d9401a341fd8b828ec4c9096dab
sauloorizaga <159079803+sauloorizaga@users.noreply.github.com>
2026-04-10 17:15:15 -0600

    Add files via upload

---
+++ visualization.m
@@ -0,0 +1,32 @@
+%Single Script to perform visualizations of GPU
+%computations from a .mat file
+tic;
+N=128;        %user changes the N value here
+M1=5;iter=1;tfinal=10;dt = 0.01; 
+a=0;b=2*pi;
+CH3D_GPU_project_2025_BCP(dt,M1,iter,tfinal,N)
+
+h=(b-a)/N;
+load('U_numerical','U');
+x=[a:h:b-h]; 
+[X,Y,Z] = meshgrid(x,x,x);
+figure(500)
+isosurface(X,Y,Z,U,-.3)
+isosurface(X,Y,Z,U,-.15);
+isosurface(X,Y,Z,U,-.05);
+isosurface(X,Y,Z,U,.05);
+isosurface(X,Y,Z,U,.15);
+isosurface(X,Y,Z,U,.3)
+
+ax = gca; 
+ax.FontSize = 14;
+camlight; lighting phong
+axis([a b a b a b])
+%title(['BHM method, Time = ' num2str(tfinal) ' , dt = ' num2str(dt)] ); 
+title(['Time = ' num2str(tfinal)] ); 
+
+a=toc;
+minutes=a/60;
+hours=a/60^2;
+minutes_hours=[minutes hours]
+max(max(max(U)))
